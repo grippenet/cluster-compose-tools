e210d0557c2aea9006b3bbde934214ebbb2be209
Clément Turbelin <clement.turbelin@gmail.com>
2021-04-16 12:39:23 +0200

    allow variable substitution in variable content

--- env.py
+++ env.py
@@ -1,18 +1,31 @@
-
+# Very simple environment template parser
+# Only substitute name variable to a predermined set of known values from env.json
 import glob
 import json
+
+def parse(content, data): 
+    for name, value in data.items():
+        var = '{{' + name + '}}'
+        content = content.replace(var, value)
+    return content
 
 def update_template(file, data):
     with open(file, 'r') as f:
         content = f.read()
         f.close()
-        for name, value in data.items():
-            var = '{{' + name + '}}'
-            content = content.replace(var, value)
+    content = parse(content, data)
     return content 
+
+def update_data(data):
+    d = {}
+    for name, value in data.items():
+        d[name] = parse(value, data)
+    return d        
 
 with open('env.json','r') as fp:
     data = json.load(fp)
+
+data = update_data(data)
 
 for file in glob.glob('env/*.tpl'):
     target = file.replace('.tpl', '.env')
